9e62de9969ce81f9863a491e7a16b8b69db845aa
NayimWd <nayim.wd@gmail.com>
2026-05-16 18:07:52 +0600

    placing glob center

--- components/ui/grid/Globe.tsx
+++ components/ui/grid/Globe.tsx
@@ -12,7 +12,7 @@ export function GlobeDemo() {
 
       <div
   className="absolute left-1/2 -translate-x-1/2 pointer-events-none"
-  style={{ width: "100%", aspectRatio: "1/1", top: "50%" }}  
+  style={{ width: "100%", aspectRatio: "1/1", top: "-50%" }}  
 >
         <DotLottieReact
           src="https://lottie.host/b2294b36-d9bf-4569-afbc-b78b7d14e2f2/m1b4q2bOLG.lottie"
